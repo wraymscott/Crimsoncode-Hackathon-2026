80d6a138d818a9780c935edeac1e1d801c3dae7d
Wray Scott <wraymscott@gmail.com>
2026-02-21 13:55:16 -0800

    fixed up down movement

--- undercooked/assets/scenes/player_controller.gd
+++ undercooked/assets/scenes/player_controller.gd
@@ -18,7 +18,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up")
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := Vector3(input_dir.x, 0, input_dir.y).normalized()
 	
 
