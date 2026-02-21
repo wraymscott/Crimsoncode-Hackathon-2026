68e08c817f2cd6726a2fc7a3e8c4effcf01c7a54
Wray Scott <wraymscott@gmail.com>
2026-02-21 15:55:55 -0800

    Revert "removed jump velocity"
    This reverts commit f892d0e1230973f6907b5e0a34ea0392f1c56ca2.

--- undercooked/assets/scenes/player_controller.gd
+++ undercooked/assets/scenes/player_controller.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 @onready var state_machine = animation_tree["parameters/playback"]
 
 const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
 
 
 func _physics_process(delta: float) -> void:
