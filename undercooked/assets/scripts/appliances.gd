ecec5e6cc5bd879624c36b85122925e144fdefbb
Wray Scott <wraymscott@gmail.com>
2026-02-21 21:50:10 -0800

    p

--- undercooked/assets/scripts/appliances.gd
+++ undercooked/assets/scripts/appliances.gd
@@ -15,5 +15,22 @@ var ingredient3_fulfilled = false
 func _ready() -> void:
 	pass # Replace with function body.
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	# Get all bodies currently inside the box
+	var bodies = area.get_overlapping_bodies()
+	
+	for body in bodies:
+		if body.is_in_group("player") and Input.get_action_raw_strength("player_interact0"):
+			var character = body
+			var instanced_item =  contains_item.instantiate()
+			character.pick_up_item(instanced_item)
+			print("test")
+		if body.is_in_group("player") and Input.get_action_raw_strength("player_interact1"):
+			var character = body
+			var instanced_item =  contains_item.instantiate()
+			character.pick_up_item(instanced_item)
+
+
 func use_ingredient(ingredient):
 	pass
